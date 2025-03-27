86d415def04c55c4b8240b9cb203ece7ea68b0c0
Ishimwediane <ishimwediane400@gmail.com>
2025-03-27 22:34:09 +0200

    Fix deployment

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    tailwindcss(),
-    react(),
-    
+    tailwindcss(),  // Tailwind CSS support
+    react(),        // React plugin support
   ],
- base:"/Shehope/",
-})
+  base: "/Shehope/", // Base path for GitHub Pages deployment
+});
